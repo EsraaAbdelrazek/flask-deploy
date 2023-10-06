50e909798f3899bbb9360a364dacdc91a38cc814
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2023-10-06 09:41:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask 
-app = flask (__name__)
-from flask import Flask
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, swimmer!</p>"
-if __name__ =="__main__"
-    app.run()
+# Load the pre-trained machine learning model
+model = joblib.load('water_quality_model.pkl')
+
+@app.route('/')
+def hello():
+    return 'Welcome to the Water Quality Prediction API!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get input data from the request as JSON
+        data = request.get_json()
+
+        # Ensure that the request contains valid JSON data
+        if not data:
+            return jsonify({'error': 'Invalid JSON data'}), 400
+
+        # Convert JSON data to a DataFrame for prediction
+        input_data = pd.DataFrame(data, index=[0])
+
+        # Make predictions using the loaded model
+        predictions = model.predict(input_data)
+
+        # Return the predictions as JSON
+        return jsonify({'predictions': predictions.tolist()})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
